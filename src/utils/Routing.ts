e294ddd92c9858d80a7d88d9f50bf9357b9a247e
verzac <benjamin.tanone@gmail.com>
2019-08-19 16:00:30 +1000

    added Google Analytics; added react-ga

--- src/utils/Routing.ts
+++ src/utils/Routing.ts
@@ -1,6 +1,26 @@
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory, Location, Action } from 'history';
+import ReactGA from 'react-ga';
 
 const history = createBrowserHistory();
+ReactGA.initialize('UA-145929509-1');
+track(history.location);
+
+history.listen((location, action) => {
+  track(location);
+});
+
+function shouldTrack(): boolean {
+  return window.location.hostname !== 'localhost';
+}
+
+function track(location: Location) {
+  if (shouldTrack()) {
+    ReactGA.pageview(location.pathname);
+  } else {
+    console.debug('Skipping tracking using GA.');
+    return;
+  }
+}
 
 function goTo(route: string): void {
   console.log(`Redirecting to ${route}...`);
