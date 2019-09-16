277b146e8aed066cd18ced83773242be8f4738e6
verzac <benjamin.tanone@gmail.com>
2019-09-17 08:49:36 +1000

    fixed React not found bug

--- src/pages/ForRecruiterPage.tsx
+++ src/pages/ForRecruiterPage.tsx
@@ -3,6 +3,7 @@ import TechnologiesPage from './micro-pages/TechnologiesPage';
 import LanguagePage from './micro-pages/LanguagePage';
 import ExperiencePage from './micro-pages/ExperiencePage';
 import PageSection from '../components/PageSection';
+import React from 'react';
 
 const ForRecruiterPage: React.FC = () => {
   return (
